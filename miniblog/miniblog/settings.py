bb7d2c74541ad668d096a979c66d9346c168f03e
manish <kumarmanishkumar12@gmail.com>
2022-10-15 20:25:18 +0530

    image issue fix in home page  and dashboard page

--- miniblog/miniblog/settings.py
+++ miniblog/miniblog/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = STATIC_URL
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static")
+]
 
 
 # Default primary key field type
