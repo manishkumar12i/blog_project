7e69e61080e07c4ced8f813883892c7613dc7fb6
Manish kumar <kumarmanishkumar12@gmail.com>
2022-10-18 20:34:01 +0530

    add forget password page in login form  and its template in blog folder

--- miniblog/blog/views.py
+++ miniblog/blog/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render , redirect
+from django.http import HttpResponseRedirect , HttpResponse
 from blog.forms import SignUpForm, LoginForm, PostForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from blog.models import Post,AboutUs
 from django.core.paginator import Paginator
 from django.contrib.auth.models import Group
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
+from django.core.mail import send_mail, BadHeaderError
 
 
 def home(request):
@@ -152,3 +160,32 @@ def search(request):
         search = request.GET.get('search')
         post = Post.objects.all().filter(title__contains=search)
         return render(request, 'blog/search.html',{'post': post})
+
+# forget password 
+def password_reset_request(request):
+	if request.method == "POST":
+		password_reset_form = PasswordResetForm(request.POST)
+		if password_reset_form.is_valid():
+			data = password_reset_form.cleaned_data['email']
+			associated_users = User.objects.filter(Q(email=data))
+			if associated_users.exists():
+				for user in associated_users:
+					subject = "Password Reset Requested"
+					email_template_name = "main/password/password_reset_email.txt"
+					c = {
+					"email":user.email,
+					'domain':'127.0.0.1:8000',
+					'site_name': 'Website',
+					"uid": urlsafe_base64_encode(force_bytes(user.pk)),
+					"user": user,
+					'token': default_token_generator.make_token(user),
+					'protocol': 'http',
+					}
+					email = render_to_string(email_template_name, c)
+					try:
+						send_mail(subject, email, 'admin@example.com' , [user.email], fail_silently=False)
+					except BadHeaderError:
+						return HttpResponse('Invalid header found.')
+					return redirect ("/password_reset/done/")
+	password_reset_form = PasswordResetForm()
+	return render(request, "blog/password_reset.html", context={"password_reset_form":password_reset_form})
--- miniblog/miniblog/urls.py
+++ miniblog/miniblog/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from blog import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -32,4 +33,9 @@ urlpatterns = [
     path('search/',views.search,name="search"),
     path('update_post/<int:id>/',views.update_post,name="update_post"),
     path('delete/<int:id>/',views.delete_post, name="delete"),
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='blog/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="blog/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='blog/password_reset_complete.html'), name='password_reset_complete'),      
+
 ]
