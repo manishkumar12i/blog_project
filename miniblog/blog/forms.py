b8e30d32d0d4e1c689cbfb4e20b72b4f05e944ef
Manish kumar <kumarmanishkumar12@gmail.com>
2023-03-15 00:30:50 +0530

    add set password form in forms.py custom file

--- miniblog/blog/forms.py
+++ miniblog/blog/forms.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from blog .models import Post
 from django.utils.translation import gettext,gettext_lazy as _
 from captcha.fields import CaptchaField
+from django.contrib.auth.password_validation import validate_password
+
 
 class SignUpForm(UserCreationForm):
     password1 = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
@@ -20,6 +22,8 @@ class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus':True, 'class':'form-control'}))
     password = forms.CharField(label=_("Password"),strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password', 'class':'form-control'}))
 
+ 
+
 
 
 class PostForm(forms.ModelForm):
@@ -28,3 +32,21 @@ class PostForm(forms.ModelForm):
         fields = ['title','description','photo']
         labels = {'title':'Title','description':'Description','photo':'Upload Image'}
         widgets = {'title':forms.TextInput(attrs={'class':'form-control'}),'description':forms.Textarea(attrs={'class':'form-control'})}
+
+
+# set password form 
+class SetPasswordForm(forms.Form):
+
+    New_Password = forms.CharField(widget=forms.PasswordInput, validators=[validate_password])
+    Confirm_password = forms.CharField(widget=forms.PasswordInput, validators=[validate_password])
+
+    error_messages = {
+       'password_mismatch': ("The two password fields didn't match."),
+       }
+
+    class Meta:
+        model = User
+        fields = ('password',)
+
+    def __init__(self,*args, **kwargs):
+       super(SetPasswordForm, self).__init__(*args, **kwargs)
