5d1ab4150ae5c0b34498e99f9dcf35f896e03755
Manish kumar <kumarmanishkumar12@gmail.com>
2023-03-19 22:52:16 +0530

    remove of set password custom form

--- miniblog/blog/admin.py
+++ miniblog/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post,AboutUs,Footer,SubscribedUsers,ContactUs,Set_Password
+from blog.models import Post,AboutUs,Footer,SubscribedUsers,ContactUs
 
 @admin.register(Post)
 class PostModelAdmin(admin.ModelAdmin):
--- miniblog/blog/forms.py
+++ miniblog/blog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UsernameField
 from django.contrib.auth.models import User
-from blog .models import Post,Set_Password
+from blog .models import Post
 from django.utils.translation import gettext,gettext_lazy as _
 from captcha.fields import CaptchaField
 
