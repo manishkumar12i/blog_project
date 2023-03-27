16e66e1c5b2c511bdcc82624d90436958747e8e7
Manish kumar <kumarmanishkumar12@gmail.com>
2023-03-27 15:40:41 +0530

    requirements.txt file and settigs file changes

--- miniblog/miniblog/settings.py
+++ miniblog/miniblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$2@3!5l&2h39ma$azl2*un-wdck*709@bp^ve=_v577^qx%@5w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
